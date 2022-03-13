ef5d237922f60cd48a5db01438b3a25abad057b7
root <2489542106@qq.com>
2022-03-13 10:43:32 +0800

    delete 6

--- src/main/java/com/example/hello/controller/HelloController.java
+++ src/main/java/com/example/hello/controller/HelloController.java
@@ -21,11 +21,4 @@ public class HelloController {
         return str+" "+"World!";
     }
 
-    @RequestMapping(value = "/hello6")
-    public String hello6(String str) {
-
-        LOGGER.info("接收的参数:{}",str);
-
-        return str+" "+"World!";
-    }
 }
